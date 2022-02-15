01dbe0f2252b6fa0e74d92c801c32bdd315a9ef0
triandamai <triannurizkillah@gmail.com>
2022-02-15 11:05:32 +0700

    feat: update project

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,13 @@
+//no-trailing-spaces
 import * as functions from "firebase-functions";
-
-import { initializeApp } from "firebase-admin"; 
-import {sendNotificationToDevice} from "./notification/broadcast"
+import {initializeApp} from "firebase-admin"; 
+import {sendNotificationToDevice} from "./notification/broadcast";
 
 initializeApp(
     functions.config().firebase
-)
+);
 
 exports.senBroadcast = functions
     .firestore
     .document("notification/{documentId}")
-    .onCreate(sendNotificationToDevice)
+    .onCreate(sendNotificationToDevice);
